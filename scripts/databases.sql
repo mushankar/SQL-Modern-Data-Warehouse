9192d5588a4c53e89ec59d811fb0dfef8e93bd36
mushankar <130592474+mushankar@users.noreply.github.com>
2025-08-10 11:59:03 +0530

    Update databases.sql

--- scripts/databases.sql
+++ scripts/databases.sql
@@ -1,3 +1,16 @@
+/*
+=================================
+CREATE DATABASE AND SCHEMAS
+=================================
+
+Script Purpose: 
+        This script creates a new database named "Datawarehouse" after checking if it already exists. If it already exists it drops the entire database and
+recreates it. And three schemas are setup after the database is created as follows {gold, silver & bronze}
+
+Warning: 
+        Running this script will drop or delete the existing data within warehouse "Datawarehouse" if it exists.
+*/
+
 USE master;
 GO
 
